3ff33b38c7ef234e524e2300cf7f33f25694b6d8
Svetlana Shcherbakova <63914437+sw-shar@users.noreply.github.com>
2022-04-24 23:20:31 +0300

    not save images

--- base_model.py
+++ base_model.py
@@ -141,16 +141,7 @@ class BaseModel(object):
                 captions.append(caption)
                 scores.append(score)
 
-                # Save the result in an image file
-                image_file = batch[l]
-                image_name = image_file.split(os.sep)[-1]
-                image_name = os.path.splitext(image_name)[0]
-                img = plt.imread(image_file)
-                plt.imshow(img)
-                plt.axis('off')
-                plt.title(caption)
-                plt.savefig(os.path.join(config.test_result_dir,
-                                         image_name+'_result.jpg'))
+              
 
         # Save the captions to a file
         results = pd.DataFrame({'image_files':test_data.image_files,
